3270fb67c05181a09b380f75be62d2733ba80167
elviswangari <elviswangare@gmail.com>
2023-08-03 19:11:21 +0300

    sql

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -29,3 +29,15 @@ PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
 print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
+#question 4
+get_db = __import__('filtered_logger').get_db
+
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()
+
