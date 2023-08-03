c4d5a0af41750c70ca5038da75f24c78dc94491b
elviswangari <elviswangare@gmail.com>
2023-08-03 16:26:29 +0300

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,11 +8,8 @@ from typing import List
 
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
-    '''
-    replace sensitive info with redacted value
-    '''
+    '''replace sensitive info with redacted value'''
     for value in fields:
         message = sub(f'{value}=.*?{separator}',
                       f'{value}={redaction}{separator}', message)
-
     return message
