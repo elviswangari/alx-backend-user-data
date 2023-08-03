10d587acec5e0c69f832223ecd3f1b4d0baf2699
elviswangari <elviswangare@gmail.com>
2023-08-03 16:06:02 +0300

    filter

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+logging module
+'''
+from re import sub
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    '''
+    replace sensitive info with redacted value
+    '''
+    for value in fields:
+        message = sub(f'{value}=.*?{separator}',
+                      f'{value}={redaction}{separator}', message)
+
+    return message
