8eefc9df23717c8444313f269ec7f2e32bf6b82a
elviswangari <elviswangare@gmail.com>
2023-08-03 19:01:22 +0300

    logger class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''replace sensitive info with redacted value'''
@@ -14,6 +17,21 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{value}=.*?{separator}',
                          f'{value}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    '''
+    logger
+    '''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -2,6 +2,8 @@
 """
 Main file
 """
+import logging
+import re
 
 filter_datum = __import__('filtered_logger').filter_datum
 
@@ -11,3 +13,19 @@ messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_bir
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
 
+#question2
+
+RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+print(formatter.format(log_record))
+
+
+#question 3
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
